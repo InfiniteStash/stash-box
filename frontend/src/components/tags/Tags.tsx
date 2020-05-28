f56568df4ab173e7c4b8e5b9cf04efbc0b017c27
Infinite <infinitekittens@protonmail.com>
2020-05-28 20:03:00 +0000

    Fixes

--- frontend/src/components/tags/Tags.tsx
+++ frontend/src/components/tags/Tags.tsx
@@ -16,7 +16,7 @@ const TagsComponent: React.FC = () => {
 
   const tags = data.queryTags.tags.map((tag) => (
     <li key={tag.id}>
-      <Link to={`/tags/${tag.name}`}>{tag.name}</Link>
+      <Link to={`/tags/${encodeURIComponent(tag.name)}`}>{tag.name}</Link>
       <span className="ml-2">{tag.description}</span>
     </li>
   ));
